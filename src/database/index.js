6610158a026bea84d6afca548efe5dd01e147a04
Leonardo Pereira <leosantospereira10@hotmail.com>
2021-12-23 15:17:14 -0300

    t

--- src/database/index.js
+++ src/database/index.js
@@ -7,10 +7,10 @@ const User = require('../models/User');
 
 const connection = new Sequelize({
     dialect: 'mysql',
-    host: process.env.host || 'ec2-174-129-252-252.compute-1.amazonaws.com',
-    username: process.env.user || 'qmdtpmuiojwxrd',
-    password: process.env.password || 'c1ba3a792ba5cae2980d8a7c938a0a755404d3b0ca982bb7719a1c4abd699e4a',
-    database: process.env.database || 'd9eb47gr6u9k44',
+    host: process.env.host,
+    username: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
     define: {
         timestamps: true,
         underscored: true,
