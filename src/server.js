e0ef01709848ce79edcd13d3987b2420243e0f9a
Leonardo Pereira <leosantospereira10@hotmail.com>
2021-12-23 15:11:11 -0300

    t

--- src/server.js
+++ src/server.js
@@ -10,6 +10,15 @@ app.use(cors());
 app.use(express.json());
 // verifica as rotas quando tiverem session
 app.use('/',routes);
-app.use('/site',express.static(path.join(path.resolve(__dirname,'..'), 'site/')))
+// app.use('/site',express.static(path.join(path.resolve(__dirname,'..'), 'site')))
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 4000);
